40fdd0c113b424be68cdbc117d1994d50998c50f
shandroid19 <shanthanucn@gmail.com>
2022-01-06 22:44:27 +0530

    project.txt added

--- routes/posts.js
+++ routes/posts.js
@@ -22,9 +22,9 @@ router.route('/')
 .options(cors.corsWithOptions,(req,res)=>{res.sendStatus(200)})
 .get(cors.cors,authenticate,(req,res)=>{
     console.log(req.username)
-    const sql = 'select * from posts where username=?'
-    con.query(sql,[req.username],function (err,result){
-        res.status(200).send(result[0]);
+    const sql = 'select * from post where username = ?'
+    con.query(sql,[req.username],function (err,result,field){
+        res.status(200).send(result);
 
     })
 })
@@ -33,7 +33,7 @@ router.route('/')
     var values = [req.username,req.body.img,req.body.caption,req.body.private]
     con.query(sql,values,function (err,result,fields){
         console.log(result)
-        res.status(200).send(result[0]);
+        res.status(200).json("successful");
     })
 })
 
@@ -52,10 +52,48 @@ router.route('/:postid')
     const sql = 'delete from post where postid=?'
     con.query(sql,[req.params.postid],function (err,result){
         console.log(result)
-        res.status(200).send(result[0]);
+        res.status(200).json("successful");
+    })
+})
+router.route('/:postid/comment')
+.post(cors.cors,authenticate,(req,res)=>{
+    const sql = 'insert into comments(postid,comment,username) values(?,?,?) '
+    con.query(sql,[req.params.postid,req.body.comment,req.username],function (err,result){
+        console.log(result)
+        console.log()
+        res.status(200).json("successful");
+    })
+})
+router.route('/comments/:commentid')
+.delete(cors.cors,authenticate,(req,res)=>{
+    const sql = 'delete from comments where commentid=?'
+    con.query(sql,[req.params.commentid],function (err,result){
+        console.log(result)
+        console.log()
+        res.status(200).json("successful");
     })
 })
 
-
+router.route('/:postid/like')
+.get(cors.cors,authenticate,(req,res)=>{
+    const sql = 'select * from likes where postid=?'
+    con.query(sql,[req.params.postid],function (err,result){
+        res.status(200).send(result[0]);
+    })
+})
+router.route('/:postid/like')
+.post(cors.cors,authenticate,(req,res)=>{
+    const sql = 'insert into likes values(?,?)'
+    con.query(sql,[req.params.postid,req.username],function (err,result){
+        res.status(200).json("successful");
+    })
+})
+router.route('/:postid/like')
+.delete(cors.cors,authenticate,(req,res)=>{
+    const sql = 'delete from likes where username=?'
+    con.query(sql,[req.username],function (err,result){
+        res.status(200).json("successful");
+    })
+})
 })
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,88 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var config = require('../config')
+var mysql = require('mysql');
+const authenticate = require('../authenticate');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: config.username,
+  password: config.password
 });
 
-router.get('/:username')
-module.exports = router;
+con.connect(function(err) { //to connect to the database 
+    if (err) throw err;
+    con.query("use social", function (err, result,fields) {
+      if (err) throw err;
+        console.log('using');
+      router.route('/follow')
+      .post(cors.cors,authenticate,(req,res)=>{
+        const sql = 'insert into followers values(?,?)';
+        con.query(sql,[req.body.username,req.username],(err,result)=>{
+            if (err) res.status(500);
+              res.status(200).json("successful");
+        })
+      })
+
+      router.route('/unfollow')
+      .post(cors.cors,authenticate,(req,res)=>{
+        const sql = 'delete from followers where follower=? and username';
+        con.query(sql,[req.username,req.body.username],(err,result)=>{
+          if (err) res.status(500);
+              res.status(200).json("successful");
+        })
+      })
+
+      router.route('/reject')
+      .post(cors.cors,authenticate,(req,res)=>{
+        const sql = 'delete from requests where touser=? and fromuser=?';
+        con.query(sql,[req.username,req.body.username],(err,result)=>{
+          if (err) res.status(500);
+              res.status(200).json("successful");
+        })
+      })
+
+      router.route('/accept')
+      .post(cors.cors,authenticate,(req,res)=>{
+        con.query('delete from requests where touser=? and fromuser=?',[req.username,req.body.username],(er,re)=>{
+        if(er) throw res.status(500);
+        const sql = 'insert into follower values(?,?)';
+        con.query(sql,[req.username,req.body.username],(err,result)=>{
+          if (err) res.status(500);
+              res.status(200).json("successful");
+        })
+      })
+      })
+
+      router.route('/request')
+      .post(cors.cors,authenticate,(req,res)=>{
+        const sql = 'insert into requests values(?,?)';
+        con.query(sql,[req.username,req.body.username],(err,result)=>{
+          if (err) res.status(500);
+              res.status(200).json("successful");
+        })
+      })
+
+      router.route('/cancel')
+      .post(cors.cors,authenticate,(req,res)=>{
+        const sql = 'delete from requests where touser=? and fromuser=?';
+        con.query(sql,[req.body.username,req.username],(err,result)=>{
+          if (err) res.status(500);
+              res.status(200).json("successful");
+        })
+      })
+
+      router.route('/search')
+      .get(cors.cors,authenticate,(req,res)=>{
+        const sql = 'select top 10 username,dp from user where username like ?%'
+        con.query(sql,[req.body.username],(err,result)=>{
+          if (err) res.status(500)
+          res.status(200).json(result[0])
+      })
+
+      });
+    })
+})
